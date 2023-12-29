72db49fcbf110665381af2fd091c0aee7b387309
rhrafiulhaque <rh.rafiul.haque@gmail.com>
2023-12-29 22:58:55 +0600

    jsonwebtoken added

--- src/app/config.ts
+++ src/app/config.ts
@@ -8,4 +8,10 @@ export default {
   database_url: process.env.DATABASE_URL,
   default_user_pass: process.env.DEFAULT_USER_PASS,
   bycrpt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jwt: {
+    jwt_secret: process.env.JWT_SECRET,
+    jwt_expire_in: process.env.JWT_EXPIRES_IN,
+    jwt_refresh_token: process.env.JWT_REFRESH_SECRET,
+    jwt_refresh_expire_in: process.env.JWT_REFRESH_EXPIRES_IN,
+  },
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt'
 import httpStatus from 'http-status'
+import jwt from 'jsonwebtoken'
 import ApiError from '../../error/ApiError'
 import { IUser } from '../users/user.interface'
 import { User } from '../users/user.model'
@@ -9,7 +10,7 @@ const logIn = async (user: IUser): Promise<IUser | null> => {
     {
       $or: [{ email }, { contactNo }],
     },
-    { password: 1, role: 1 },
+    { password: 1, role: 1 ,email:1 },
   ).lean()
   console.log(userExist)
   if (!userExist) {
@@ -18,9 +19,15 @@ const logIn = async (user: IUser): Promise<IUser | null> => {
 
   const isPasswordMatched = await bcrypt.compare(password, userExist.password)
   if (!isPasswordMatched) {
-    console.log("Hello bro");
     throw new ApiError(httpStatus.UNAUTHORIZED, 'Password Doesnot Match!')
   }
+ 
+  const accessToken = jwt.sign({
+    email:
+    role: userExist?.role
+  })
+
+
   console.log(isPasswordMatched)
   return user
 }
