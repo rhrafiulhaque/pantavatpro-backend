6d8703ea05c8eff575952dfe71b6ed59bb83e3e0
unknown <rh.rafiul.haque@gmail.com>
2023-12-30 13:33:08 +0600

    login section added

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,24 @@
 import { RequestHandler } from 'express'
 import { authService } from './auth.service'
+import config from '../../app/config'
 
 const logIn: RequestHandler = async (req, res, next) => {
   try {
     const result = await authService.logIn(req.body)
+
+    const { refreshToken, ...others } = result
+
+    const cookieOptions = {
+      secure: config.env === 'production',
+      httpOnly: true,
+    }
+
+    res.cookie('refreshToken', refreshToken, cookieOptions)
+
     res.status(200).json({
       success: true,
       message: 'User Logged In successfully!',
-      data: result,
+      data: others,
     })
   } catch (err) {
     next(err)
